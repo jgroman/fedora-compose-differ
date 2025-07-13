c0a9aa95e5db2fb8a06d4d1f4372d526bc5890e6
jgroman <jgroman@users.noreply.github.com>
2025-07-13 16:13:56 +0200

    Update tests

--- compose_diff/compose_diff_utils.py
+++ compose_diff/compose_diff_utils.py
@@ -1,5 +1,5 @@
 """
-...
+compose_diff utils module
 """
 
 import logging
@@ -29,25 +29,26 @@ URL_RPMS_JSON_TEMPLATE = "{url_root}{full_version}/compose/metadata/rpms.json"
 logger = logging.getLogger()
 
 
-class PkgRemovedModel(BaseModel):
-    name: str
-    version: str
+class PackageModel(BaseModel):
+    """Package pydantic model"""
 
-
-class PkgAddedModel(BaseModel):
     name: str
     version: str
 
 
 class PkgChangedModel(BaseModel):
+    """Package diff changed package pydantic model"""
+
     name: str
     version_from: str
     version_to: str
 
 
 class PkgDiffModel(BaseModel):
-    removed: list[PkgRemovedModel]
-    added: list[PkgAddedModel]
+    """Package diff root pydantic model"""
+
+    removed: list[PackageModel]
+    added: list[PackageModel]
     changed: list[PkgChangedModel]
 
 
@@ -80,6 +81,8 @@ def parse_compose_root(root_html: str) -> list[str]:
 
 
 def get_rpms_json_url(version: str) -> str:
+    """Generate rpms.json URL for given compose version"""
+
     return URL_RPMS_JSON_TEMPLATE.format(
         url_root=URL_COMPOSE_ROOT,
         full_version=f"Fedora-Rawhide-{version}"
@@ -89,8 +92,9 @@ def get_rpms_json_url(version: str) -> str:
 
 
 def parse_streamed_rpms_json(url_json: str, arch: str = "x86_64") -> dict[str, str]:
-    logger.debug(f"Streaming rpms.json from '{url_json}'")
+    """Parse rpms.json during its download"""
 
+    logger.debug(f"Streaming rpms.json from '{url_json}'")
     try:
         jsonfile = request.urlopen(url_json)
         result: dict[str, str] = dict()
@@ -105,14 +109,17 @@ def parse_streamed_rpms_json(url_json: str, arch: str = "x86_64") -> dict[str, s
 
 
 def diff_packages(dict_from: dict[str, str], dict_to: dict[str, str]) -> PkgDiffModel:
+    """Compute package diff from two compose builds package lists"""
+
+    logger.debug("Computing package diff")
     return PkgDiffModel(
         removed=[
-            PkgRemovedModel(name=name, version=version)
+            PackageModel(name=name, version=version)
             for (name, version) in dict_from.items()
             if name not in dict_to
         ],
         added=[
-            PkgAddedModel(name=name, version=version)
+            PackageModel(name=name, version=version)
             for (name, version) in dict_to.items()
             if name not in dict_from
         ],
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,10 +6,12 @@ import urllib.request
 import urllib.response
 
 from compose_diff.compose_diff_utils import (
+    diff_packages,
     download_url,
     parse_compose_root,
     parse_streamed_rpms_json,
-    URL_COMPOSE_ROOT,
+    PackageModel,
+    PkgChangedModel,
 )
 
 
@@ -56,7 +58,33 @@ def mock_compose_root_versions() -> list[str]:
     ]
 
 
+@pytest.fixture
+def mock_packages_from() -> dict[str, str]:
+    return {
+        "package_no_change_1": "1",
+        "package_removed_1": "1",
+        "package_changed_1": "1",
+        "package_no_change_2": "1",
+        "package_removed_2": "1",
+        "package_changed_2": "1",
+    }
+
+
+@pytest.fixture
+def mock_packages_to() -> dict[str, str]:
+    return {
+        "package_no_change_1": "1",
+        "package_added_1": "1",
+        "package_changed_1": "2",
+        "package_no_change_2": "1",
+        "package_added_2": "1",
+        "package_changed_2": "2",
+    }
+
+
 def test_download_url(monkeypatch, mock_compose_root_content):
+    """Test download_url()"""
+
     def mock_urlopen(url):
         return urllib.response.addinfourl(
             fp=io.BytesIO(mock_compose_root_content.encode()),
@@ -67,17 +95,20 @@ def test_download_url(monkeypatch, mock_compose_root_content):
 
     monkeypatch.setattr(urllib.request, "urlopen", mock_urlopen)
 
-    compose_root_content = download_url(URL_COMPOSE_ROOT)
+    compose_root_content = download_url("mock-root-url")
     assert compose_root_content == mock_compose_root_content
 
 
 def test_parse_compose_root(mock_compose_root_content, mock_compose_root_versions):
+    """Test parse_compose_root()"""
     parse_result = parse_compose_root(mock_compose_root_content)
 
     assert parse_result == mock_compose_root_versions
 
 
 def test_parse_streamed_rpms_json(monkeypatch, mock_rpms_json_content):
+    """Test parse_streamed_rpms_json()"""
+
     def mock_urlopen(url):
         return urllib.response.addinfourl(
             fp=io.BytesIO(mock_rpms_json_content.encode()),
@@ -87,4 +118,39 @@ def test_parse_streamed_rpms_json(monkeypatch, mock_rpms_json_content):
         )
 
     monkeypatch.setattr(urllib.request, "urlopen", mock_urlopen)
-    packages = parse_streamed_rpms_json("xxx")
+    packages = parse_streamed_rpms_json("mock-rpms.json-url")
+
+    assert packages == {
+        "0ad": "0:0.0.26-30",
+        "0ad-data": "0:0.0.26-11",
+        "0install": "0:2.18-4",
+        "0xFFFF": "0:0.10-13",
+        "2048-cli": "0:0.9.1-23",
+    }
+
+
+def test_diff_packages(mock_packages_from, mock_packages_to):
+    """Test diff_packages()"""
+
+    diff = diff_packages(mock_packages_from, mock_packages_to)
+
+    expected_removed = [
+        PackageModel(name="package_removed_1", version="1"),
+        PackageModel(name="package_removed_2", version="1"),
+    ]
+
+    assert diff.removed == expected_removed
+
+    expected_added = [
+        PackageModel(name="package_added_1", version="1"),
+        PackageModel(name="package_added_2", version="1"),
+    ]
+
+    assert diff.added == expected_added
+
+    expected_changed = [
+        PkgChangedModel(name="package_changed_1", version_from="1", version_to="2"),
+        PkgChangedModel(name="package_changed_2", version_from="1", version_to="2"),
+    ]
+
+    assert diff.changed == expected_changed
